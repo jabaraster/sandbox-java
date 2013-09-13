c721068acb43ebc49dab06eb42ace10d36e012b4
jabaraster <jabaraster@gmail.com>
2013-09-13 12:09:04 +0900

    mainte.

--- src/main/webapp/TopPage.js
+++ src/main/webapp/TopPage.js
@@ -39,14 +39,21 @@ function showMap(pPosition) {
         center: position
     };
     var map = new maps.Map(document.getElementById("map"), opts);
+    var markers = new Markers({ map: map });
+
     var timer = null;
+    var clearTimer = function() {
+        if (timer !== null) {
+            clearTimeout(timer.key);
+            timer = null;
+        }
+    };
     maps.event.addListener(map, 'mousedown', function(e) {
         if (timer != null) {
             clearTimeout(timer.key);
         }
         var key = setTimeout(function() {
-            addMarker({
-                map: map,
+            markers.addMarker({
                 position: new maps.LatLng(e.latLng.mb, e.latLng.nb)
             });
         }, 1000);
@@ -54,14 +61,25 @@ function showMap(pPosition) {
             key: key
         };
     });
+    maps.event.addListener(map, 'mousemove', function(e) {
+        clearTimer();
+    });
     maps.event.addListener(map, 'mouseup', function(e) {
-        if (timer != null) {
-            clearTimeout(timer.key);
-        }
+        clearTimer();
+    });
+
+    $('button.current-pin').click(function() {
+        navigator.geolocation.getCurrentPosition(function(e) {
+            markers.addMarker({
+                position: new maps.LatLng(e.coords.latitude, e.coords.longitude)
+            });
+        });
     });
 }
 
 function addMarker(pArgs) {
+    new Marker(pArgs);
+    return;
     var marker = new maps.Marker({
         position: pArgs.position,
         map: pArgs.map,
@@ -96,11 +114,78 @@ function failGetCurrentPosition() {
     alert('現在地表示はサポートされません.');
 }
 
-function Marker() {
+function Marker(pArgs) {
+    var marker = new maps.Marker(pArgs);
+    var content = $('#templates > .balloon-content').clone();
+    var balloon = new maps.InfoWindow({
+        content: content.get(0)
+    });
+    var self = this;
+    maps.event.addListener(marker, 'click', function() {
+        balloon.open(pArgs.map, marker);
+        self.publish('balloonOpen', self);
+    });
+    maps.event.addListener(marker, 'dragend', function(e) {
+    });
+
+    content.find('.marker-delete').click(function() {
+        marker.setMap(null);
+        return false;
+    });
+    content.find('button').click(function() {
+        self.closeBalloon();
+        return false;
+    });
+
+    this.openBalloon = function() {
+        balloon.open(pArgs.map, marker);
+        self.publish('balloonOpen', self);
+    };
+
+    this.publish = function(pEventName, pEventArgs) {
+        pArgs[pEventName + 'Listener'](pEventArgs);
+    }
+
+    this.closeBalloon = function() {
+        balloon.close();
+    }
+
+    // ピン位置の住所の取得を試みる.
+    new maps.Geocoder().geocode({ latLng: pArgs.position }, function(pResult, pStatus) {
+        var address = content.find('input[class="address"]');
+        if (pResult.length == 0) {
+            address.attr('placeholder', '住所が取得できませんでした');
+        } else {
+            address.val(pResult[0].formatted_address);
+        }
+    });
+
+    // バルーンを開く.
+    // ただし、ピンが落ちるアニメーションをきれいに見せるために、１秒遅延させる.
+    setTimeout(function() { self.openBalloon(); }, 1000);
 }
 
-function Markers() {
-    var _gMarkers = [];
+function Markers(pArgs) {
+    var map = pArgs.map;
+    var markers = []; /* googleのものでではないMarker型 */
+    var balloonOpeningMarker = null;
+    var balloonOpenListener = function(pMarker) {
+        if (balloonOpeningMarker === pMarker) {
+            return;
+        }
+        if (balloonOpeningMarker !== null) {
+            balloonOpeningMarker.closeBalloon();
+        }
+        balloonOpeningMarker = pMarker;
+    };
+
+    this.addMarker = function(pArgs) {
+        pArgs.balloonOpenListener = balloonOpenListener;
+        pArgs.map = map;
+        if (!('animation' in pArgs)) pArgs.animation = maps.Animation.DROP;
+        if (!('draggable' in pArgs)) pArgs.draggable = true;
+        markers.push(new Marker(pArgs));
+    }
 }
 
 })();
